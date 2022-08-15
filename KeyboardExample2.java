2699b1a2c225e0dcf04140aa96f5837a9a3b9213
sudipto <fedev.sudipto@gmail.com>
2022-08-15 14:43:58 +0530

    adding graphics and keyboard events

---
+++ KeyboardExample2.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+@SuppressWarnings("serial")
+public class KeyboardExample2 extends JPanel {
+
+    public KeyboardExample2(){
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("keyReleased="+KeyEvent.getKeyText(e.getKeyCode()));
+            }
+        };
+        addKeyListener(listener);
+        setFocusable(true);
+    }
+}
