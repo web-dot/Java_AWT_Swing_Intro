adaea9151ae1094862503fd31d27cb1efe8e30a4
sudipto <fedev.sudipto@gmail.com>
2022-08-15 19:04:55 +0530

    ball collision with racquet

--- Ball.java
+++ Ball.java
@@ -1,7 +1,8 @@
 
-import java.awt.Graphics2D;
+import java.awt.*;
 
 public class Ball {
+    private static final int DIAMETER = 30;
     int x = 0;
     int y = 0;
     int xa = 1;
@@ -15,18 +16,29 @@ public class Ball {
     void move(){
         if(x + xa < 0)
             xa = 1;
-        if(x + xa > game.getWidth() - 30)
+        if(x + xa > game.getWidth() - DIAMETER)
             xa = -1;
         if(y + ya < 0)
             ya = 1;
-        if(y + ya > game.getHeight() - 30)
+        if(y + ya > game.getHeight() - DIAMETER)
+            game.gameOver();
+        if(collision()){
             ya = -1;
-
+            y = game.racquet.getTopY() - DIAMETER;
+        }
         x = x + xa;
         y = y + ya;
     }
 
+    private boolean collision(){
+        return game.racquet.getBounds().intersects(getBounds());
+    }
+
     public void paint(Graphics2D g){
-        g.fillOval(x, y, 100, 100);
+        g.fillOval(x, y, 30, 30);
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, DIAMETER, DIAMETER);
     }
 }
--- Game2.java
+++ Game2.java
@@ -1,16 +1,39 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Game2 extends JPanel{
 
     Ball ball = new Ball(this);
+    Racquet racquet = new Racquet(this);
+
+    public Game2(){
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                racquet.keyPressed(e);
+            }
+        });
+        setFocusable(true);
+    }
 
     private void move(){
         ball.move();
+        racquet.move();
     }
 
     @Override
@@ -20,13 +43,19 @@ public class Game2 extends JPanel{
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
         ball.paint(g2d);
+        racquet.paint(g2d);
+    }
+
+    public void gameOver(){
+        JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_OPTION);
+        System.exit(ABORT);
     }
 
     public static void main(String[] args) throws InterruptedException{
         JFrame frame = new JFrame("Mini Tennis");
         Game2 game = new Game2();
         frame.add(game);
-        frame.setSize(300, 300);
+        frame.setSize(300, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
